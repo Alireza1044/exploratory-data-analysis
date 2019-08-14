0ac1f50ef9457e6a73a0e75358fe10b53d3da2fa
Alireza <nili_505077@yahoo.comgit config --global user.email nili_505077@yahoo.com>
2019-08-14 17:25:33 +0430

    Fix a bug

--- plot6.R
+++ plot6.R
@@ -2,9 +2,13 @@ library(dplyr)
 library(ggplot2)
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
-sub <- NEI %>% subset(type == "ON-ROAD" & (fips == "24510" | fips == "06037"))
+baltimore <- NEI %>% subset(type == "ON-ROAD" & fips == "24510")
+california <- NEI %>% subset(type == "ON-ROAD" & fips == "06037")
+california$fips <- "Los Angeles County"
+baltimore$fips <- "Baltimore City"
+sub <- rbind(baltimore,california)
 png("plot6.png")
-g <- ggplot(data = baltimore, aes(factor(year),y = Emissions)) + geom_bar(stat = "identity") + facet_grid(. ~ fips) + ylab("Total PM25 Emission (Tons)") + xlab("Year")
+g <- ggplot(data = sub, aes(factor(year),y = Emissions)) + geom_bar(stat = "identity") + facet_grid(. ~ fips) + ylab("Total PM25 Emission (Tons)") + xlab("Year")
 g <- g + geom_bar(stat = "identity")
 g
 dev.off()
